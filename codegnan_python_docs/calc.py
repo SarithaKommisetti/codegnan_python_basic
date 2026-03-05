2b364236611ce22173054833129db02e0adcf7a2
SarithaKommisetti <sarithakommisetti392000@gmail.com>
2026-03-05 16:14:04 +0530

    Add files via upload

---
+++ codegnan_python_docs/calc.py
@@ -0,0 +1,92 @@
+'''#Fee calculator
+a=int(input("Enter hostel fee:"))
+b=int(input("Enter course fee:"))
+print("Fee Calculator")
+print("##############")
+print("Hostel fee:",a)
+print("Course fee:",b)
+print("Total fee:",a+b)'''
+
+#if condition by using comparison
+#<,>,<=,>=,!=,==
+'''a=2
+b=5
+if a<b:
+    print("true")'''
+
+'''a=2
+b=5
+if a>b:
+    print("true")
+
+a=10
+b=20
+if a<=b:
+    print("true")
+
+a=12
+b=15
+if a>=b:
+    print("true")
+
+a=12
+b=10
+if a!=b:
+    print("true")
+
+a=10
+b=20
+if a==b:
+    print("true")
+
+a="python"
+if a=="python":
+    print("true")
+
+a="python"
+if a=="java":
+    print("true")
+
+
+#runtime_input
+
+a=int(input())
+b=int(input())
+if a<b:
+    print("true")
+    
+a=int(input())
+b=int(input())
+if a>b:
+    print("false")
+
+a=int(input())
+b=int(input())
+if a<=b:
+    print("false")
+
+a=int(input())
+b=int(input())
+if a>=b:
+    print("true")
+
+a=int(input())
+b=int(input())
+if a!=b:
+    print("true")
+
+a=str(input("Enter value of a"))
+if a=="python":
+    print("true")
+
+a=str(input("Enter value of a"))
+b=str(input("Enter value of b"))
+if a==b:
+    print("true")
+
+a=str(input())
+b=str(input())
+if a!=b:
+    print("false")'''
+
+
