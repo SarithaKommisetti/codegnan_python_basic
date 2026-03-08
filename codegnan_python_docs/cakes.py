bec210b7688ba84d00c625e01830d84d8528f190
SarithaKommisetti <sarithakommisetti392000@gmail.com>
2026-03-08 12:37:56 +0530

    Add files via upload

---
+++ codegnan_python_docs/cakes.py
@@ -0,0 +1,10 @@
+print("Bakery")
+price=int(input("Enter the price:"))
+if price==1200:
+    print("Red velvet Cake")
+elif price==800:
+    print("Choco Almond")
+elif price==600:
+    print("Butterscotch")
+else:
+    print("Sorry cake is not available")
---
+++ codegnan_python_docs/pizza.py
@@ -0,0 +1,15 @@
+name=str(input("Enter name:"))
+if name=="BBQ":
+    print(1000)
+elif name=="Crispy chicken Pizza":
+    print(800)
+elif name=="Paneer Pizza":
+    print(600)
+elif name=="Corn Pizza":
+    print(400)
+elif name=="French Fries and coke":
+    print(200)
+else:
+    print("Sorry not available")
+    
+    
