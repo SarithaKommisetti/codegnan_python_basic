06105b7d66823b777a3099027569f7718b32b3ca
SarithaKommisetti <sarithakommisetti392000@gmail.com>
2026-03-16 12:43:53 +0530

    Add files via upload

---
+++ range_tasks.py
@@ -0,0 +1,21 @@
+'''for i in range(0,20,2):
+    print(i)'''
+
+'''for i in range(5,50,5):
+    print(i,end=" ")'''
+
+'''for i in range(3,30,3):
+    print(i)'''
+
+'''while True:
+    num=int(input("Enter the number:"))
+    if num in range(91,101):
+        print("Grade-A")
+    elif num in range(81,91):
+        print("Grade-B")
+    elif num in range(71,81):
+        print("Grade-C")
+    elif num in range(50,71):
+        print("Grade-D")
+    else:
+        print("Fail")'''
---
+++ while.py
@@ -0,0 +1,161 @@
+#while loop
+'''a=3
+b=7
+while a<b:
+    print(a)'''
+
+'''a=5
+while a>1:
+    print(a)
+    a-=1'''
+
+'''a=5
+while a>1:
+    a-=1
+    print(a)'''
+    
+'''a=1
+while a<5:
+    print(a)
+    a+=1'''
+
+'''a=1
+while a<5:
+    a+=1
+    print(a)'''
+
+'''while True:
+    a=int(input("Enter the a value:"))
+    if a%2==0:
+        print("even")
+    else:
+        print("odd")'''
+
+'''while True:
+    age=int(input("Enter age:"))
+    if age>=18:
+        print("Eligible to vote")
+    else:
+        print("Not eligible to vote")'''
+
+#range()
+#start-stop-step
+
+'''for i in range(10,20):
+    print(i,end=" ")'''
+
+#break
+'''a=10
+while a>1:
+    print(a)
+    a=a-1
+    if a==5:
+        break'''
+'''a=10
+while a>1:
+    a=a-1
+    print(a)
+    if a==5:
+        break'''
+
+'''a=10
+while a>1:
+    a=a-1
+    if a==5:
+        break
+    print(a)'''
+
+'''a=10
+while a>=1:
+    print(a)
+    a=a-1
+    if a==5:
+        break'''
+
+'''for i in range(25):
+    if i==15:
+        break
+    print(i)'''
+
+'''a="python"
+if a=="h":
+    break
+print(a)'''#error
+
+'''a="python"
+for i in a:
+    if i=="h":
+        break
+    print(i)'''
+
+#continue
+'''a=20
+while a>1:
+    print(a)
+    a=a-1
+    if a==10:
+        continue'''
+
+'''a=20
+while a>1:
+    a=a-1
+    print(a)
+    if a==10:
+        continue'''
+
+'''a=20
+while a>1:
+    a=a-1
+    if a==10:
+        continue
+    print(a)'''
+
+'''a=20
+while a>1:
+    print(a)
+    a=a-1
+    if a==10:
+        break
+    if a==10:
+        continue'''
+
+#pass
+'''a=20
+while a>2:
+    print(a)
+    a=a-1
+    if a==10:
+        pass'''
+
+'''for i in range(10):
+    if i==5:
+        pass
+    print(i)'''
+
+a="code"
+for i in a:
+    if i=="d":
+        pass
+    print(i)
+
+
+    
+    
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
